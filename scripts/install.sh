ff5eddaab0f533596feb735bdf00799523c120fa
ragavellur <raga.vellur@gmail.com>
2026-07-27 19:43:13 +0530

    fix(install): add /simulation/ Nginx proxy route for simulation service
    Both HTTPS and HTTP Nginx config blocks now include location /simulation/
    proxying to localhost:3002. Required for frontend simulation API calls
    to route through the domain instead of hitting localhost directly.

--- scripts/install.sh
+++ scripts/install.sh
@@ -375,6 +375,14 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
     }
 
+    location /simulation/ {
+        proxy_pass http://127.0.0.1:3002/;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
     location /geocode/ {
         proxy_pass http://127.0.0.1:8080/;
         proxy_set_header Host \$host;
@@ -428,6 +436,14 @@ server {
         proxy_pass http://127.0.0.1:5001/;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
+    }
+
+    location /simulation/ {
+        proxy_pass http://127.0.0.1:3002/;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
     location /geocode/ {
